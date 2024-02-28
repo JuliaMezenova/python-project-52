308f4fe19b4291ebd2b89ece9bb6821fa36822fc
Julia Mezenova <julia21moon86@mail.ru>
2024-02-28 16:10:42 +0500

    task_manager/settings.py, try4

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if DEBUG is False:
+if DEBUG == 'False':
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
