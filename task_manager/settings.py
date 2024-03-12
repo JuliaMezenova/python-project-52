d650b83d793b0ad64c61f47cf140805bafa79344
Julia Mezenova <julia21moon86@mail.ru>
2024-03-12 16:28:37 +0500

    modified  task_manager/settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -89,8 +89,8 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {
     'default': dj_database_url.config(
-        os.getenv("DATABASE_URL"), f"sqlite:///{BASE_DIR/'db.sqlite3'}",
-        conn_max_age=600
+        os.getenv("DATABASE_URL"), f"sqlite:///{BASE_DIR/'db.sqlite3'}"
+        # conn_max_age=600
     )
 }
 # Password validation
