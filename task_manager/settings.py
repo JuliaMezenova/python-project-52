33c747e1e92eeafddd09ce5345e26c62d60410c4
Julia Mezenova <julia21moon86@mail.ru>
2024-03-12 10:10:07 +0500

    modified task_manager.yml and settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -88,16 +88,25 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default=os.getenv('DATABASE_URL'), # f"sqlite:///{BASE_DIR/'db.sqlite3'}"
-        conn_max_age=600
-    )
-}
-if 'test' in sys.argv or 'coverage' in sys.argv:
-    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
-
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.getenv('DATABASE_URL'),  # f"sqlite:///{BASE_DIR/'db.sqlite3'}"),
+#         conn_max_age=600
+#     )
+# }
+# if 'test' in sys.argv or 'coverage' in sys.argv:
+#     DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+if os.getenv("DATABASE_URL"):
+    # use db from env if it exists
+    DATABASES = {"default": dj_database_url.config(default=os.getenv("DATABASE_URL"))}
+# else:
+if 'test' in sys.argv or 'coverage' in sys.argv:    
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
