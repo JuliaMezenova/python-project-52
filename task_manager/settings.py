dd1254e6b5b84e6c5423cc40fee04a1439a94817
Julia Mezenova <julia21moon86@mail.ru>
2024-03-19 14:40:41 +0500

    modified settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -90,16 +90,12 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-if os.getenv("DATABASE_URL"):
-    # use db from env if it exists
-    DATABASES = {"default": dj_database_url.config(default=os.getenv("DATABASE_URL"))}
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR/'db.sqlite3'}")
+    )
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
